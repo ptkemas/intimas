ad395cd07aa9803f9abbfc948cba423b9d5f1e60
ptkemas <100741032+ptkemas@users.noreply.github.com>
2026-07-28 17:11:36 +0700

    Add files via upload

---
+++ assets/index-DSvPzTfU.js
@@ -0,0 +1,1 @@
+import{ba as o,X as a,bb as r}from"./index-BoK8UmIJ.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
