0c8b7f33472b1fa91c60c94d77ae54fb8ffec909
ptkemas <100741032+ptkemas@users.noreply.github.com>
2026-07-31 16:36:54 +0700

    Delete assets directory

--- assets/index-DSvPzTfU.js
+++
@@ -1,1 +0,0 @@
-import{ba as o,X as a,bb as r}from"./index-BoK8UmIJ.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
